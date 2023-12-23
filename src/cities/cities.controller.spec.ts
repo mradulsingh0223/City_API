649f5925aad8d0340c68f0740adb3189e103c3a7
mradulsingh0223 <mradul.singh.min21@itbhu.ac.in>
2023-12-24 00:10:33 +0530

    UPDATED tests for controllers and services

--- src/cities/cities.controller.spec.ts
+++ src/cities/cities.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CitiesController } from './cities.controller';
 import { CitiesService } from './cities.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { City } from './entities/city.entity';
 
 describe('CitiesController', () => {
   let controller: CitiesController;
@@ -8,7 +10,13 @@ describe('CitiesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CitiesController],
-      providers: [CitiesService],
+      providers: [
+        CitiesService,
+        {
+          provide: getRepositoryToken(City),
+          useValue: {}, // Mock or use an in-memory repository for testing
+        },
+      ],
     }).compile();
 
     controller = module.get<CitiesController>(CitiesController);
--- src/cities/cities.service.spec.ts
+++ src/cities/cities.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CitiesService } from './cities.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { City } from './entities/city.entity';
 
 describe('CitiesService', () => {
   let service: CitiesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CitiesService],
+      providers: [
+        CitiesService,
+        {
+          provide: getRepositoryToken(City),
+          useValue: {}, // Mock or use an in-memory repository for testing
+        },
+      ],
     }).compile();
 
     service = module.get<CitiesService>(CitiesService);

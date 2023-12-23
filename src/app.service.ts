3a141ffb95464de0568d2e393095a282106f2df2
mradulsingh0223 <mradul.singh.min21@itbhu.ac.in>
2023-12-23 23:07:37 +0530

    ADDED CRUD operations to cities

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Vibhu!';
+    return 'Go to /cities route to GET cities!';
   }
 }

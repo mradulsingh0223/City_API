125ad3c50326eb14248da962bdbb93d93773dc0e
mradulsingh0223 <mradul.singh.min21@itbhu.ac.in>
2023-12-24 01:04:51 +0530

    ADDED timeout

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -7,6 +7,7 @@ describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
+    jest.setTimeout(10000);
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
